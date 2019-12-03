60cf0907482747e75edb094177d22fe0328a009b
Victor Yip <victor.yip@mail.utoronto.ca>
2019-12-03 16:51:29 -0500

    Corrected model classification output size

--- models.py
+++ models.py
@@ -95,9 +95,9 @@ class resnet152(nn.Module):
         num_ftrs = self.model.fc.in_features
         print(num_ftrs)
         self.model.fc = nn.Linear(num_ftrs,1024)
-        self.fc2 = nn.Linear(1024,18)
+        self.fc2 = nn.Linear(1024,17)
         self.bn = nn.BatchNorm1d(1024)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
         # self.fc3 = nn.Linear(512,18)
 
     def forward(self,x):
@@ -116,9 +116,9 @@ class vgg19bn(nn.Module):
         old.pop()
         old.append(nn.Linear(num_ftrs,2048))
         self.model.classifier = nn.Sequential(*old)
-        self.fc2 = nn.Linear(2048,18)
+        self.fc2 = nn.Linear(2048,17)
         self.bn = nn.BatchNorm1d(2048)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
         # self.fc3 = nn.Linear(1024,18)
 
     def forward(self,x):
@@ -140,9 +140,9 @@ class alex(nn.Module):
         old.append(nn.Linear(num_ftrs,2048))
         print(num_ftrs)
         self.model.classifier = nn.Sequential(*old)
-        self.fc2 = nn.Linear(2048,18)
+        self.fc2 = nn.Linear(2048,17)
         self.bn = nn.BatchNorm1d(2048)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
 
     def forward(self,x):
         # x = self.model(x)
@@ -159,8 +159,8 @@ class dense161(nn.Module):
             param.requires_grad = False
         num_ftrs = self.model.classifier.in_features
         self.model.classifier = nn.Linear(num_ftrs,1104)
-        self.fc2 = nn.Linear(1104, 18)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.fc2 = nn.Linear(1104, 17)
+        self.bn2 = nn.BatchNorm1d(17)
         self.bn = nn.BatchNorm1d(1104)
         #self.fc3 = nn.Linear(552,18)
 
@@ -180,9 +180,9 @@ class resnext101(nn.Module):
             param.requires_grad = False
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs,1024)
-        self.fc2 = nn.Linear(1024, 18)
+        self.fc2 = nn.Linear(1024, 17)
         self.bn = nn.BatchNorm1d(1024)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
 
     def forward(self,x):
         x = F.relu(self.bn(self.model(x)))
@@ -197,9 +197,9 @@ class google(nn.Module):
             param.requires_grad = False
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, 512)
-        self.fc2 = nn.Linear(512, 18)
+        self.fc2 = nn.Linear(512, 17)
         self.bn = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
 
     def forward(self,x):
         x = F.relu(self.bn(self.model(x)))
@@ -217,9 +217,9 @@ class wres101(nn.Module):
             param.requires_grad = False
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs,1024)
-        self.fc2 = nn.Linear(1024,18)
+        self.fc2 = nn.Linear(1024,17)
         self.bn = nn.BatchNorm1d(1024)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
     def forward(self,x):
         x = F.relu(self.bn(self.model(x)))
         x = self.bn2(self.fc2(x))
@@ -234,9 +234,9 @@ class shuffle(nn.Module):
             param.requires_grad = False
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, 512)
-        self.fc2 = nn.Linear(512, 18)
+        self.fc2 = nn.Linear(512, 17)
         self.bn = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(18)
+        self.bn2 = nn.BatchNorm1d(17)
 
     def forward(self,x):
         x = F.relu(self.bn(self.model(x)))
@@ -343,7 +343,7 @@ class TransferEnsembleFrozenLight(nn.Module):
         for model in self.marray:
             for param in model.parameters():
                 param.requires_grad = False
-        self.fc1 = nn.Linear(136,68)
+        self.fc1 = nn.Linear(51,68)
         self.fc2 = nn.Linear(68,18)
         self.bn2 = nn.BatchNorm1d(18)
         self.bn1 = nn.BatchNorm1d(68)
