0a9a80f90869a7c9cae7992e386e5f9c73c5a237
Victor Yip <victor.yip@mail.utoronto.ca>
2019-12-03 02:38:29 -0500

    Added ensemble loop for simultaneous traiing

--- transfergeneration.py
+++ transfergeneration.py
@@ -103,15 +103,15 @@ valloader = DataLoader(valid,shuffle=True,batch_size=bs,pin_memory=False)
 testloader = DataLoader(test,shuffle=True,batch_size=bs,pin_memory=False)
 
 torch.manual_seed(1)
-net1 = resnet152()
-net2 = vgg19bn()
-net3 = dense161()
-net4 = alex()
-net5 = resnext101()
-net6 = google()
-net7 = wres101()
-net8 = shuffle()
-net_arr = [net1,net2,net3,net4,net5,net6,net7,net8]
+net0 = resnet152()
+net1 = vgg19bn()
+net2 = dense161()
+net3 = alex()
+net4 = resnext101()
+net5 = google()
+net6 = wres101()
+net7 = shuffle()
+net_arr = [net0,net1,net2,net3,net4,net5,net6,net7]
 
 optimizer_arr = []
 for i in range(0,len(net_arr)):
@@ -142,7 +142,7 @@ for tloopnum in range(len(net_arr)):
             j += 1
             print('Model #: ', tloopnum)
             print('Epoch #: ', epoch)
-            print('Batch #: ', j)
+            print('Batch #: ', batchperepoch)
             inputs, labels = data
             optimizer.zero_grad()
             outputs = net(inputs)
@@ -214,4 +214,140 @@ for tloopnum in range(len(net_arr)):
     # # print('y_ground', y_ground)
     # # print('y_pred', y_pred)
     # print(confusion_matrix(y_ground,y_pred))
-    torch.save(net, 'model%d.pt'%tloopnum)
+    # torch.save(net, 'model%d.pt' % tloopnum)
+    if tloopnum == 0:
+        torch.save(net, 'resnet.pt')
+    elif tloopnum == 1:
+        torch.save(net, 'vgg.pt')
+    elif tloopnum == 2:
+        torch.save(net, 'dense.pt')
+    elif tloopnum == 3:
+        torch.save(net, 'alex.pt')
+    elif tloopnum == 4:
+        torch.save(net, 'resnext.pt')
+    elif tloopnum == 5:
+        torch.save(net, 'google.pt')
+    elif tloopnum == 6:
+        torch.save(net, 'wres.pt')
+    elif tloopnum == 7:
+        torch.save(net, 'shuffle.pt')
+    else:
+        print('ERROR: Non-existent model!')
+
+# Now, all transfer models have been finetuned for our problem.
+# Using these, we will attempt to assemble
+net0 = torch.load('resnet.pt')
+net1 = torch.load('vgg.pt')
+net2 = torch.load('dense.pt')
+net3 = torch.load('alex.pt')
+net4 = torch.load('resnext.pt')
+net5 = torch.load('google.pt')
+net6 = torch.load('wres.pt')
+net7 = torch.load('shuffle.pt')
+
+net = TransferEnsembleFrozen(net0,net1,net2,net3,net4,net5,net6,net7)
+optimizer = optim.Adam(net.parameters(),lr=0.001)
+criterion = nn.CrossEntropyLoss()
+
+val_acc_tot = []
+train_acc_tot = []
+test_acc_tot = []
+loss_tot = []
+val_loss_tot = []
+test_loss_tot = []
+n_tot = []
+f1_tot = []
+recall_tot = []
+j = 0
+for epoch in range(e_num):
+    net = net.train()
+    sum_loss = 0
+    running_loss = 0
+    running_acc = 0
+    batchperepoch = 0
+    for i, data in enumerate(trainloader, 0):
+        batchperepoch += 1
+        j += 1
+        print('Epoch #: ', epoch)
+        print('Batch #: ', batchperepoch)
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        # print('batch label',labels)
+        # print('batch_label long',labels.long())
+        loss = criterion(outputs.squeeze(), torch.max(labels.long(), 1)[1])
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.detach()
+        running_acc += accuracy(outputs, labels)[0]
+
+    # if j%step== 0:
+    # if epoch%1 == epoch:
+    net = net.eval()
+    print(running_loss / batchperepoch)
+    loss_tot.append(running_loss / batchperepoch)
+    # t_acc = accuracy(outputs,labels)[0]
+    t_acc = running_acc / batchperepoch
+    train_acc_tot.append(t_acc)
+    # acc_tot.append(acc[0])
+    n_tot.append(epoch)
+    running_loss = 0
+    temp = evaluate(net, valloader)
+    testeval = evaluate(net, testloader)
+    val_loss_tot.append(temp[1])
+    val_acc_tot.append(temp[0])
+    test_acc_tot.append(testeval[0])
+    test_loss_tot.append(testeval[1])
+    print('Train Acc: ', t_acc)
+    print('Validation Acc: ', temp[0])
+    print('Testing Acc: ',testeval[0])
+    print('Train Loss: ', running_loss / batchperepoch)
+    print('Validation Loss: ', temp[1])
+    print('Testing Loss: ', testeval[1])
+    y_ground = []
+    y_pred = []
+    for j, batch in enumerate(valloader, 1):
+        valid_train, valid_label = batch
+        predict = net(valid_train.float())
+        predictions = predict.detach()
+        index = 0
+        for pred in predictions:
+            p_val, p_clas = torch.max(pred, 0)
+            v_val, v_clas = torch.max(valid_label[index], 0)
+            y_pred.append(p_clas.item())
+            y_ground.append(v_clas.item())
+            index += 1
+    f1 = f1_score(y_ground, y_pred, average='micro')
+    recall = recall_score(y_ground, y_pred, average='micro')
+    print('F1 :', f1)
+    print('Recall: ', recall)
+    f1_tot.append(f1)
+    recall_tot.append(recall)
+print('Finished Training')
+print('Train Acc: ', train_acc_tot)
+print('Val Acc: ', val_acc_tot)
+print('Test Acc: ', test_acc_tot)
+print('Train Loss: ', loss_tot)
+print('Valid Loss: ', val_loss_tot)
+print('Test Loss: ', test_loss_tot)
+print('Recall Score: ', recall_tot)
+print('F1 Score: ', f1_tot)
+y_ground = []
+y_pred = []
+
+for j, batch in enumerate(testloader, 1):
+    valid_train, valid_label = batch
+    predict = net(valid_train.float())
+    predictions = predict.detach()
+    index = 0
+    for pred in predictions:
+        p_val, p_clas = torch.max(pred, 0)
+        v_val, v_clas = torch.max(valid_label[index], 0)
+        y_pred.append(p_clas.item())
+        y_ground.append(v_clas.item())
+        index += 1
+print('y_ground', y_ground)
+print('y_pred: ', y_pred)
+print(confusion_matrix(y_ground, y_pred))
+print('F1 :', f1_score(y_ground, y_pred))
+print('Recall: ', recall_score(y_ground, y_pred))
